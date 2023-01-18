7ada757d96dba70d8e39bbaafe3fe225762e0a46
alexander <alexander@klippel.se>
2023-01-18 21:43:59 +0100

    Update lastnight.py

--- lastnight.py
+++ lastnight.py
@@ -7,8 +7,10 @@ def lastnight(bot, trigger):
     # Fetch the data from the web
     r = requests.get('https://lastnight.in/sweden/')
     soup = BeautifulSoup(r.text, 'html.parser')
+
+    # Extract the number of days and the source of the last major incident
     days = soup.find('strong').text.strip()
+    source = soup.find('a', href=True, text='Source').get('href')
 
     # Reply with the data
-    bot.say(f'It has been {days} day(s) since the last major incident in Sweden. (https://lastnight.in/sweden/)')
-
+    bot.say(f'It has been {days} day(s) since the last major incident in Sweden (https://lastnight.in). Latest incident: {source}')
