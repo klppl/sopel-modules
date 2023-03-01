ca1a4485e88d29042be60a54a734ca72c3309636
alexander <alexander@klippel.se>
2023-03-01 21:51:44 +0100

    Create chatgpt.py

---
+++ chatgpt.py
@@ -0,0 +1,25 @@
+import sopel
+import openai
+import requests
+
+openai.api_key = "API_KEY"
+
+@sopel.module.commands('chatgpt')
+def openai_chat(bot, trigger):
+    text = trigger.group(2)
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": text,
+            }
+        ],
+    )
+    response = completion['choices'][0]['message']['content']
+
+    response = response.encode("utf-8")
+    response = requests.post("https://dumpinen.com", data=response)
+    chatgpt_output = response.text
+
+    bot.say(f'chatgpt: {chatgpt_output}')
