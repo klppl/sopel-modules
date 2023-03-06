0a4afd7235e9e6eab7fa17a80e81386da218b53c
alexander <alexander@klippel.se>
2023-03-06 11:44:53 +0100

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -1,2 +1,29 @@
+import sopel
+import openai
+import requests
+import textwrap
+import apikeys
+
+openai.api_key = OPENAI_API_KEY
+
+@sopel.module.commands('chatgpt')
+def openai_chat(bot, trigger):
+    text = trigger.group(2)
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": text,
+            }
+        ],
+    )
+    response = completion['choices'][0]['message']['content']
+
+    response = response.encode("utf-8")
+    response = textwrap.fill(response, 400)
+    response = requests.post("https://dumpinen.com", data=response)
+    chatgpt_output = response.text
+
 
     bot.say(f'chatgpt: {chatgpt_output}')
