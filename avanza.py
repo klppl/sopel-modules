b8967bfa7b2e58c96aa8e563ca371752b846611e
alexander <alexander@klippel.se>
2023-03-13 08:28:05 +0100

    updated with index command

--- avanza.py
+++ avanza.py
@@ -70,3 +70,48 @@ def avanzafind(bot, trigger):
             link_displays.append(hit["link"]["linkDisplay"])
     
     bot.say(", ".join(link_displays))
+
+@module.commands('index', 'i')
+def avanzaindex(bot, trigger):
+
+    omxs30 = "19002"
+    nasdaq = "19006"
+    dax = "18981"
+    nikkei225 = "18997"
+    index_list = [omxs30, nasdaq, dax, nikkei225]
+
+    for index in index_list:
+        data = json.loads(requests.get("https://www.avanza.se/_mobile/market/orderbooklist/" + index).text
+
+        stock_name = data[0]["name"]
+        last_price = data[0]["lastPrice"]
+        currency = data[0]["currency"]
+        price_three_months_ago = data[0]["priceThreeMonthsAgo"]
+        
+        try:
+            highest_price = data[0]["highestPrice"]
+        except:
+            highest_price = "N/A"
+            
+        try:
+            lowest_price = data[0]["lowestPrice"]
+        except:
+            lowest_price = "N/A"
+
+        change_in_number = data[0]["change"]
+        change_in_percent = data[0]["changePercent"]
+        if int(change_in_percent) <0:
+            change_in_percent = formatting.color(str(change_in_percent) + '%', formatting.colors.RED)
+        elif change_in_percent >0:
+            change_in_percent = formatting.color(str(change_in_percent) + '%', formatting.colors.GREEN)
+
+        change_in_months = round((float(last_price) - float(price_three_months_ago))/float(price_three_months_ago)*100,1)
+        if int(change_in_months) <0:
+            change_in_months = formatting.color(str(change_in_months) + '%', formatting.colors.RED)
+        elif change_in_months >0:
+            change_in_months = formatting.color(str(change_in_months) + '%', formatting.colors.GREEN)
+
+        total_volume_traded = data[0]["totalVolumeTraded"]
+        
+        bot.say(f'{stock_name} | {last_price} {currency} | Idag: {change_in_percent} ({change_in_number} {currency}) | 3 mån: {change_in_months} ({price_three_months_ago} {currency}) | Volla: {lowest_price} - {highest_price} {currency} | Volym: {total_volume_traded}')
+
