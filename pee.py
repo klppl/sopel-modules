a32a16d0e30940b6ec11e0af65c08226aeb5917a
alexander <alexander@klippel.se>
2023-03-14 07:41:09 +0100

    update

--- pee.py
+++ pee.py
@@ -1,10 +1,11 @@
 from sopel import module
+from sopel import formatting
 
 @module.commands('pee')
 def pee_alias(bot, trigger):
     nickname = trigger.group(2)
-    bot.say("8===D - - -  -")
-    bot.say("                 `")
-    bot.say("                  '")
-    bot.say("                  '")
-    bot.say(f"                {nickname}")
+    bot.say(formatting.color("8===D", "orange") + formatting.color("~~ ~", "yellow"))
+    bot.say(formatting.color("                 `", "yellow"))
+    bot.say(formatting.color("                  '", "yellow"))
+    bot.say(formatting.color("                  '", "yellow"))
+    bot.say(formatting.color("                " + nickname, "blue"))
