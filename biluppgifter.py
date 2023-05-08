f12f68b2f9a8fb51a6b9a90f69fd23a3d5ccfa2f
alexander <alexander@klippel.se>
2023-05-08 21:26:40 +0200

    Update biluppgifter.py

--- biluppgifter.py
+++ biluppgifter.py
@@ -1,62 +1,33 @@
-import sopel.module
-from sopel.config.types import StaticSection, ValidatedAttribute
+import re
 import requests
 from bs4 import BeautifulSoup
-import re
+import sopel.module
 
 TAG_RE = re.compile(r'<[^>]+>')
 
-
-class VehicleInfoSection(StaticSection):
-    token = ValidatedAttribute('token')
-
-
-def configure(config):
-    config.define_section('vehicle_info', VehicleInfoSection, validate=False)
-    config.vehicle_info.configure_setting('token', 'Enter your Discord token:')
-
-
-def setup(bot):
-    bot.config.define_section('vehicle_info', VehicleInfoSection)
-
-
-def removeHTML(text):
+def remove_html_tags(text):
     return TAG_RE.sub('', text)
 
-
-def GetInfoAbout(reg):
+def get_vehicle_info(reg):
     try:
-        URL = 'https://biluppgifter.se/fordon/' + reg
-        page = requests.get(URL)
+        url = f'https://biluppgifter.se/fordon/{reg}'
+        page = requests.get(url)
         soup = BeautifulSoup(page.content, 'html.parser')
-        ownerLink = soup.findAll("a", {"class": "gtm-merinfo"})
-        OwnerLink = str(ownerLink)
-        fabikat = soup.findAll("h1", {"class": "card-title"})
-        fabrikat = str(fabikat)
-        Fabrikat = removeHTML(fabrikat)
-        Fabrikat1 = "".join(Fabrikat.splitlines())
-        Fabrikat2 = Fabrikat1.replace('[', '')
-        FabrikatOrig = Fabrikat2.replace(']', '')
+        owner_link = soup.findAll("a", {"class": "gtm-merinfo"})
+        owner_link = str(owner_link)
+        fabrikat = soup.findAll("h1", {"class": "card-title"})
+        fabrikat = str(fabrikat)
+        fabrikat = remove_html_tags(fabrikat).strip('[]')
 
-        description = soup.findAll("img", {"src": "/images/maker/"})
-        print(description)
+        owner_link = owner_link.replace("[<a class=\"gtm-merinfo\" href=\"", '')
+        owner_link = owner_link.replace("\" rel=\"follow\" target=\"_blank\">Visa kompletta ägaruppgifter på Merinfo.se</a>]", '')
 
-        OwnerLink = "".join(OwnerLink.splitlines())
-
-        OwnerLink1 = OwnerLink.replace("[<a class=\"gtm-merinfo\" href=\"", '')
-        OwnerLink2 = OwnerLink1.replace("\" rel=\"follow\" target=\"_blank\">Visa kompletta ägaruppgifter på Merinfo.se</a>]", '')
-
-        return ("Regnummer: " + reg + " | " +
-                "Fabrikat: " + FabrikatOrig + " | " +
-                "Ägare: " + OwnerLink2 + " | " +
-                "Länk till bil: " + "https://biluppgifter.se/fordon/" + reg.upper())
+        return f"Regnummer: {reg} | Fabrikat: {fabrikat} | Ägare: {owner_link} | Länk till bil: https://biluppgifter.se/fordon/{reg.upper()}"
 
     except Exception as e:
         return e
 
-
-
 @sopel.module.commands('bil')
 def regnr(bot, trigger):
     regnr = trigger.group(2).upper()
-    bot.say(GetInfoAbout(regnr))
+    bot.say(get_vehicle_info(regnr))
