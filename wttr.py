ee7c3ae2f2945bc9722aa4367081d07a1281da84
alexander <alexander@klippel.se>
2023-07-10 09:56:34 +0200

    Update wttr
    Fixed weather where sunset data was missing

--- wttr.py
+++ wttr.py
@@ -1,7 +1,6 @@
 import sopel
 import requests
 
-
 @sopel.module.commands('wttr', 'weather')
 def weather(bot, trigger):
     city = trigger.group(2)
@@ -10,8 +9,13 @@ def weather(bot, trigger):
     if not city:
         bot.say("Du glömde att ange en stad.")
         return
-    url = f"https://wttr.in/{city}?lang=sv&format=%l:%c+%C+Temp+%t+Luftfuktighet:+%h+UV:+%u++Soluppgång:+%S+%m+Solnedgång:+%s"
-    response = requests.get(url)
+    url_with_sun = f"https://wttr.in/{city}?lang=sv&format=%l:%c+%C+Temp+%t+Luftfuktighet:+%h+UV:+%u++Soluppgång:+%S+%m+Solnedgång:+%s"
+    url_without_sun = f"https://wttr.in/{city}?lang=sv&format=%l:%c+%C+Temp+%t+Luftfuktighet:+%h+UV:+%u"
+    response = requests.get(url_with_sun)
+    
+    if response.status_code != 200:
+        response = requests.get(url_without_sun)
+        
     if response.status_code == 200:
         bot.say(response.text + " | [https://wttr.in/" + city + ".png?lang=sv]")
     else:
