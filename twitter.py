9307528d89c120fa0a72dbdee1c301440c98d594
alexander <alexander@klippel.se>
2024-02-21 20:25:17 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -10,15 +10,30 @@ DOMAIN_REGEX = r"https?://(?:m(?:obile)?\.)?(?:twitter|x)\.com/([\w\d]+)/status/
 app = Twitter("your_session_info")
 app.start(username, password)
 
+def guess_media_link(item):
+
+    variants = item.get('streams', [])
+
+    if not variants:
+        return item['media_url_https']
+
+    variants.sort(key=lambda k: k.get('bitrate', 0))
+
+    return variants[-1]['url']
+
 @module.rule(DOMAIN_REGEX)
 def fetch_tweet(bot, trigger):
-
     tweet_url = trigger.group(0)
-
+    
     try:
         tweet = app.tweet_detail(tweet_url)
+        message = f"@{tweet['author']['username']}: {tweet['text']}"
 
-      message = f"Tweet: {tweet['text']}"
+        if 'media' in tweet: 
+            for item in tweet['media']:
+                media_link = guess_media_link(item)
+                message += f" [media: {media_link}]"
+
         bot.say(message)
     except Exception as e:
         bot.say("Sorry, I couldn't fetch the tweet details.")
